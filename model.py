f67185b5569bf77ba5a239f7ca2a77203cd8509e
TeoEchavarria <echavarria.0042@gmail.com>
2024-05-12 21:53:56 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,33 +1,88 @@
 import pandas as pd
-import joblib
+from joblib import dump, load
+import os
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
-def create_model(df, outcome_col, age_group, gender):
-    column_transformer = ColumnTransformer(
-    transformers=[
-        ('cat', OneHotEncoder(), ['age_group', 'gender'])
-    ],
-    remainder='passthrough'
-)
-
-    pipeline = Pipeline(steps=[
-    ('preprocessor', column_transformer),
-    ('regressor', LogisticRegression(max_iter=1000))
-    ])
-    
-    X = df[['age_group', 'gender']]
+def create_model(df, outcome_col, age_group, gender, name):   
+    df['interaction'] = df['age_group'] + '_' + df['gender']
+    X = df[['age_group', 'gender', 'interaction']]
     y = df[outcome_col]
     
-    # Training the model
-    pipeline.fit(X, y)
+    def add_interaction(df):
+        df = df.copy()
+        df['interaction'] = df['age_group'] + "_" + df['gender']
+        return df
+    
+    column_transformer = ColumnTransformer(
+        transformers=[
+            ('cat', OneHotEncoder(handle_unknown='ignore'), ['age_group', 'gender', 'interaction'])
+        ],
+        remainder='passthrough'
+    )
 
-    input_data = pd.DataFrame({'age_group': [age_group], 'gender': [gender]})
-    prediction = pipeline.predict(input_data)[0]
-    return prediction
+    rf_pipeline = Pipeline([
+        ('preprocessor', column_transformer),
+        ('classifier', RandomForestClassifier(n_estimators=100))
+    ])
+    
+    gbm_pipeline = Pipeline([
+        ('preprocessor', column_transformer),
+        ('classifier', GradientBoostingClassifier(n_estimators=100))
+    ])
+        
+    # Training the model
+    rf_pipeline.fit(X, y)
+    gbm_pipeline.fit(X, y)
+    
+    dir_path_rm = f'models/rf/{name}'
+    dir_path_gbm = f'models/gbm/{name}'
+
+    # Check if the directory exists
+    if not os.path.exists(dir_path_rm):
+        os.makedirs(dir_path_rm)
+        print("Directory created:", dir_path_rm)
+    if not os.path.exists(dir_path_gbm):
+        os.makedirs(dir_path_gbm)
+        print("Directory created:", dir_path_gbm)
+
+        
+    dump(rf_pipeline, f'models/rf/{name}/{outcome_col}.joblib')  # Save the model to a file
+    dump(gbm_pipeline, f'models/gbm/{name}/{outcome_col}.joblib')
+
+    # input_data = pd.DataFrame({'age_group': [age_group], 'gender': [gender], 'interaction': [age_group + '_' + gender]})
+    
+    # try:
+    #     rf_prediction = rf_pipeline.predict(input_data)
+    #     gbm_prediction = gbm_pipeline.predict(input_data)
+    # except Exception as e:
+    #     print("An error occurred during prediction:", e)
+    return None #(rf_prediction+gbm_prediction)/2
+
+def predict(outcome_col, age_group, gender, name):
+    rf_model_loaded = load(f'models/rf/{name}/{outcome_col}.joblib')
+    gbm_model_loaded = load(f'models/rf/{name}/{outcome_col}.joblib')
+
+    # Now you can use the loaded model to make predictions
+    # Prepare input data as before
+    input_data = pd.DataFrame({
+        'age_group': [age_group],
+        'gender': [gender],
+        'interaction': [f'{age_group}_{gender}']
+    })
+
+    # Predict using the loaded models
+    rf_prediction = rf_model_loaded.predict(input_data)[0]
+    gbm_prediction = gbm_model_loaded.predict(input_data)[0]
+    
+    return (rf_prediction+gbm_prediction)/2
+
+
 
 links = {
     "rat" :"https://www.animallama.com/wp-content/uploads/2018/03/rat-behavior-1.jpg", 
@@ -48,24 +103,7 @@ links = {
 }
     
 
-def model_train(df, age_group, gender, name):
-    df = df.astype(float)
-    df = df.T
-    df.columns = ['win', 'lose', 'unknown']
-    df = df.reset_index()
-    df.rename(columns={'index': 'group'}, inplace=True)
-    
-    mapping = {
-    'total': ('all', 'all'),
-    'women': ('female', 'all'),
-    'man': ('male', 'all'),
-    '18-34': ('all', '18-34'),
-    '35-54': ('all', '35-54'),
-    '55': ('all', '55')
-    }
-
-    df['gender'], df['age_group'] = zip(*df['group'].map(mapping))
-    df = df[(df['gender'] != 'all') | (df['age_group'] != 'all')]
-    models = {outcome: create_model(df, outcome, age_group, gender) for outcome in ['win', 'lose', 'unknown']}
+def model_train(age_group, gender, name):
+    models = {outcome: predict(outcome, age_group, gender, name) for outcome in ['win', 'lose', 'unknown']}
     models["imageUrl"] = links[name]
     return models
